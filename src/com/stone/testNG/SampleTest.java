357b4b92643536fcd0aa40bcabc0b390538c2aac
Donghua <shidongwa@sina.com>
2014-09-15 17:00:20 +0800

    TestNG IReporter接口

---
+++ src/com/stone/testNG/SampleTest.java
@@ -0,0 +1,21 @@
+package com.stone.testNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+    @Test
+    public void testMethodOne(){
+        Assert.assertTrue(true);
+    }
+	  
+    @Test
+    public void testMethodTwo(){
+	Assert.assertTrue(false);
+    }
+	  
+    @Test(dependsOnMethods={"testMethodTwo"})
+        public void testMethodThree(){
+        Assert.assertTrue(true);
+    }
+}
