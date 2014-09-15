f6c6d8339d521f12db031b07438d814792b2ca7c
Donghua <shidongwa@sina.com>
2014-09-15 22:37:02 +0800

    update。

--- src/com/stone/testNG/CustomReporter.java
+++ src/com/stone/testNG/CustomReporter.java
@@ -2,11 +2,14 @@ package com.stone.testNG;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.testng.IReporter;
 import org.testng.ISuite;
 import org.testng.ISuiteResult;
 import org.testng.ITestContext;
+import org.testng.ITestResult;
+import org.testng.internal.TestResult;
 import org.testng.xml.XmlSuite;
 
 public class CustomReporter implements IReporter {
@@ -21,16 +24,29 @@ public class CustomReporter implements IReporter {
 			Map<String, ISuiteResult> suiteResults = suite.getResults();
 			for (ISuiteResult sr : suiteResults.values()) {
 				ITestContext tc = sr.getTestContext();
-				System.out.println("Passed tests for suite '" + suiteName
-						+ "' is:" + tc.getPassedTests().getAllResults().size());
-				System.out.println("Failed tests for suite '" + suiteName
-						+ "' is:" + tc.getFailedTests().getAllResults().size());
-				System.out
-						.println("Skipped tests for suite '" + suiteName
-								+ "' is:"
-								+ tc.getSkippedTests().getAllResults().size());
+				System.out.println("Passed tests for suite '" + suiteName + "' is:" + tc.getPassedTests().getAllResults().size());
+				System.out.println("Failed tests for suite '" + suiteName + "' is:" + tc.getFailedTests().getAllResults().size());
+				System.out.println("Skipped tests for suite '" + suiteName + "' is:" + tc.getSkippedTests().getAllResults().size());	
+				
+				Set<ITestResult> set = tc.getPassedTests().getAllResults();
+				for(ITestResult r : set){
+					System.out.println(r.getName());
+					System.out.println(r.getTestName());
+					System.out.println(r.getInstanceName());
+					System.out.println(r.getMethod().getMethodName());
+					System.out.println(r.getClass().getCanonicalName());
+				}
+				
+//				ITestResult faked = new TestResult();
 
 			}
+			
+			Set<String> keySet = suiteResults.keySet();
+			for(String key : keySet){
+				System.out.println("key=" + key);
+				System.out.println("value=" + suiteResults.get(key));
+			}
+			//suite.getResults()
 		}
 	}
 }
