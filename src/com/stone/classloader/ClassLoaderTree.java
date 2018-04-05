7becfa7c99f7dd5e7f207afa08c283e51a7a306e
donghua <10189895@qq.com>
2018-04-05 08:15:43 +0800

    NoClassDefFoundError and ClassNotFoundException

--- src/com/stone/classloader/ClassLoaderTree.java
+++ src/com/stone/classloader/ClassLoaderTree.java
@@ -1,5 +1,19 @@
 package com.stone.classloader;
 
+/**
+ compile -> run code -> delete ClassToBeRemoved -> run codes then you can see below exception
+
+ Exception in thread "main" java.lang.ClassNotFoundException: com.stone.classloader.ClassToBeRemoved
+ at java.net.URLClassLoader.findClass(URLClassLoader.java:381)
+ at java.lang.ClassLoader.loadClass(ClassLoader.java:424)
+ at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:331)
+ at java.lang.ClassLoader.loadClass(ClassLoader.java:357)
+ at java.lang.Class.forName0(Native Method)
+ at java.lang.Class.forName(Class.java:264)
+ at com.stone.classloader.TestClassNotFoundException.main(TestClassNotFoundException.java:14)
+ in TestClassNotFoundException
+
+ */
 public class ClassLoaderTree { 
 
     public static void main(String[] args) { 
--- src/com/stone/classloader/TestNoClassDefFoundError.java
+++ src/com/stone/classloader/TestNoClassDefFoundError.java
@@ -1,5 +1,18 @@
 package com.stone.classloader;
 
+/**
+ * compile -> run code -> delete ClassToBeRemoved -> run codes then you can see below exception
+ *
+ *
+Exception in thread "main" java.lang.NoClassDefFoundError: com/stone/classloader/ClassToBeRemoved
+		at com.stone.classloader.TestNoClassDefFoundError.main(TestNoClassDefFoundError.java:14)
+		Caused by: java.lang.ClassNotFoundException: com.stone.classloader.ClassToBeRemoved
+		at java.net.URLClassLoader.findClass(URLClassLoader.java:381)
+		at java.lang.ClassLoader.loadClass(ClassLoader.java:424)
+		at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:331)
+		at java.lang.ClassLoader.loadClass(ClassLoader.java:357)
+		... 1 more
+*/
 public class TestNoClassDefFoundError {
 
 	private ClassToBeRemoved testCls;
