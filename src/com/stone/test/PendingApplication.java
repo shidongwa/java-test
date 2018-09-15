454bb7150945638b9a5f71488e05ddae97f872c3
stone.shi <10189895@qq.com>
2018-09-15 11:28:32 +0800

    pending java app

---
+++ src/com/stone/test/PendingApplication.java
@@ -0,0 +1,9 @@
+package com.stone.test;
+
+import java.io.IOException;
+
+public class PendingApplication {
+    public static void main(String[] args) throws IOException {
+        System.in.read(); // #7
+    }
+}
