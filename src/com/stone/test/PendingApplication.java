82a104bb968251d984a6dba8e11710073586639b
stone.shi <10189895@qq.com>
2018-10-14 07:10:04 +0800

    pending application input

--- src/com/stone/test/PendingApplication.java
+++ src/com/stone/test/PendingApplication.java
@@ -1,9 +1,16 @@
 package com.stone.test;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class PendingApplication {
     public static void main(String[] args) throws IOException {
-        System.in.read(); // #7
+        System.out.println("press any character and exit");
+
+        // solution 1
+//        System.in.read(); // #7
+
+        // solution 2
+        new Scanner(System.in).nextLine();
     }
 }
