667c6b9113f839e479f896ff54dca45640985caf
stone <10189795@qq.com>
2021-03-08 09:23:52 +0800

    二分搜索变种，支持最左，最右，第一个大于等于，最后一个小于等于

--- src/com/stone/algorithm/search/BinarySearch.java
+++ src/com/stone/algorithm/search/BinarySearch.java
@@ -15,7 +15,7 @@ public class BinarySearch {
      * @param value
      * @return
      */
-    public int bsearch(int[] a, int n, int value) {
+    public int bsearch_LeftMost(int[] a, int n, int value) {
         int low = 0;
         int high = n - 1;
         while (low <= high) {
@@ -27,6 +27,74 @@ public class BinarySearch {
             } else {
                 if ((mid == 0) || (a[mid - 1] != value)) return mid; // 最左逼近
                 else high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 二分搜索最右边的元素
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int bsearch_RightMost(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid =  low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                if ((mid == n - 1) || (a[mid + 1] != value)) return mid; // 往右边逼近
+                else low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 第一个大于等于元素
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int bsearch_GE(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid =  low + ((high - low) >> 1);
+            if (a[mid] >= value) {
+                if ((mid == 0) || (a[mid - 1] < value)) return mid;
+                else high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 最后一个小于等于
+     * @param a
+     * @param n
+     * @param value
+     * @return
+     */
+    public int bsearch_LE(int[] a, int n, int value) {
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid =  low + ((high - low) >> 1);
+            if (a[mid] > value) {
+                high = mid - 1;
+            } else {
+                if ((mid == n - 1) || (a[mid + 1] > value)) return mid;
+                else low = mid + 1;
             }
         }
         return -1;
